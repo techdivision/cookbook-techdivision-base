5d73e8a189ff546ff798da53861bfef39470fa71
Robert Lemke <robert@typo3.org>
2014-03-07 08:49:08 +0100

    [TASK] Add packages "zip", "mc" and "ca-certificates"
    Change-Id: I2006a80b6a50a0fbcbaff84891adfb7b8855f237

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,33 +10,33 @@
 #
 
 execute "locale-gen" do
-	action :nothing
-	command "locale-gen --purge"
+  action :nothing
+  command "locale-gen --purge"
 end
 
 case node["platform_family"]
 when "debian"
-	file "/etc/locale.gen" do
-		action :create
-		owner "root"
-		group "root"
-		mode "0644"
-		content node["techdivision-base"]["locales"].join("\n") + "\n"
-		notifies :run, "execute[locale-gen]", :immediate
-	end
+  file "/etc/locale.gen" do
+    action :create
+    owner "root"
+    group "root"
+    mode "0644"
+    content node["techdivision-base"]["locales"].join("\n") + "\n"
+    notifies :run, "execute[locale-gen]", :immediate
+  end
 else
-	file "/var/lib/locales/supported.d/en" do
-		action :delete
-	end
+  file "/var/lib/locales/supported.d/en" do
+    action :delete
+  end
 
-	file "/var/lib/locales/supported.d/local" do
-		action :create
-		owner "root"
-		group "root"
-		mode "0644"
-		content node["techdivision-base"]["locales"].join("\n") + "\n"
-		notifies :run, "execute[locale-gen]", :immediate
-	end
+  file "/var/lib/locales/supported.d/local" do
+    action :create
+    owner "root"
+    group "root"
+    mode "0644"
+    content node["techdivision-base"]["locales"].join("\n") + "\n"
+    notifies :run, "execute[locale-gen]", :immediate
+  end
 end
 
 #
@@ -49,4 +49,20 @@ administrators.each do |administrator|
     key administrator["sshKey"]
     user "root"
   end
+end
+
+#
+# Packages you always need
+#
+
+package "ca-certificates" do
+  action :install
+end
+
+package "zip" do
+  action :install
+end
+
+package "mc" do
+  action :install
 end
